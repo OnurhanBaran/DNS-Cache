76780d8c500b1d9c724457f9439d6b20d5012903
OnurhanBaran <onurhanbaran1@outlook.com>
2024-09-22 14:30:12 +0300

    Dns istemci projesi

---
+++ DNS Cache/Form1.Designer.cs
@@ -0,0 +1,223 @@
+﻿
+namespace neetpro146
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(9, 7);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(302, 79);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "bilgisayarın bilgileri";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(143, 50);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(35, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "label4";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(143, 21);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "label3";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(11, 50);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(94, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "BİLGİSAYAR ADI:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(11, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(63, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "IP ADRESİ:";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AllowUserToResizeColumns = false;
+            this.dataGridView1.AllowUserToResizeRows = false;
+            this.dataGridView1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 92);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.Size = new System.Drawing.Size(299, 276);
+            this.dataGridView1.TabIndex = 2;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(317, 7);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(132, 17);
+            this.checkBox1.TabIndex = 3;
+            this.checkBox1.Text = "gerçek zamanlı kontrol";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(317, 32);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(479, 342);
+            this.listBox1.TabIndex = 4;
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.AllowUserToAddRows = false;
+            this.dataGridView2.AllowUserToDeleteRows = false;
+            this.dataGridView2.AllowUserToResizeColumns = false;
+            this.dataGridView2.AllowUserToResizeRows = false;
+            this.dataGridView2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(12, 374);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.ReadOnly = true;
+            this.dataGridView2.Size = new System.Drawing.Size(299, 274);
+            this.dataGridView2.TabIndex = 5;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(317, 380);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(197, 49);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "1.yapilacak test edilmesi";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Location = new System.Drawing.Point(327, 449);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(456, 121);
+            this.listBox2.TabIndex = 7;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(586, 380);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(197, 49);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "2.yapilacak test edilmesi";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 660);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.groupBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "NEETPRO 146 PROJE SİBER GÜVENLİK UYGULAMASI";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ DNS Cache/Form1.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;  // Process sınıfını kullanmak için
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;  // Socket sınıfını kullanmak için
+using System.ServiceProcess;  // ServiceController sınıfını kullanmak için
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace neetpro146
+{
+    public partial class Form1 : Form
+    {
+        string izlenecek = "Dnscache"; // DNS İstemcisi
+        int sirano = 0;
+        bool durum;
+        ServiceController service2;
+        string bilgisayarAdi;
+        string ipAdresi;
+        Dictionary<string, DateTime> processStartTimes = new Dictionary<string, DateTime>();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            int sn = 0, aktif = 0, pasif = 0;
+            int i;
+
+            // Bilgisayarın temel bilgileri listelenmektedir
+            bilgisayarAdi = Dns.GetHostName();
+            label4.Text = bilgisayarAdi;
+            ipAdresi = Dns.GetHostByName(bilgisayarAdi).AddressList[0].ToString();
+            label3.Text = ipAdresi;
+
+            // Grid olarak service hizmetleri listelenmektedir
+            dataGridView1.ColumnCount = 5;
+            dataGridView1.Columns[0].Name = "No";
+            dataGridView1.Columns[0].Width = 20;
+            dataGridView1.Columns[1].Name = "Adı";
+            dataGridView1.Columns[2].Name = "Ekran";
+            dataGridView1.Columns[3].Name = "Tipi";
+            dataGridView1.Columns[4].Name = "Aktifmi";
+
+            foreach (ServiceController service in ServiceController.GetServices())
+            {
+                string serviceName = service.ServiceName;
+                string serviceDisplayName = service.DisplayName;
+                string serviceType = service.ServiceType.ToString();
+                string status = service.Status.ToString();
+                if (status.Substring(0, 1) == "R") { aktif++; } else { pasif++; }
+                if (serviceName == izlenecek)
+                {
+                    sirano = sn;
+                    service2 = service;
+                    durum = status.Substring(0, 1) == "R";
+                }
+                dataGridView1.Rows.Add(sn++, serviceName, serviceDisplayName, serviceType, status);
+            }
+
+            // Arka planda çalışan programların listesi
+            dataGridView2.ColumnCount = 2;
+            dataGridView2.Columns[0].Name = "No";
+            dataGridView2.Columns[0].Width = 20;
+            dataGridView2.Columns[1].Name = "Program";
+            Process[] p = Process.GetProcesses();
+            for (i = 0; i < p.Length; i++)
+            {
+                dataGridView2.Rows.Add(i, p[i].ProcessName.ToString());
+                // processStartTimes[p[i].ProcessName] = p[i].StartTime; // Bu satırı kaldırdık
+            }
+
+            // Ekran çıktısı olarak listBox1 kullanılmaktadır
+            listBox1.Items.Clear();
+            listBox1.Items.Add("Program aktif oldu>>> " + DateTime.Now);
+            listBox1.Items.Add(sn + " adet servisten, aktif servis sayısı: " + aktif);
+            listBox1.Items.Add("Pasif servis sayısı: " + pasif);
+            listBox1.Items.Add("İzlenecek servis adı (sıra no:" + sirano + "): " + izlenecek);
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            timer1.Enabled = checkBox1.Checked;
+            listBox1.Items.Add("Gerçek zamanlı kontrol: " + checkBox1.Checked);
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            // Zaman bazlı kontrol.... Gerçek zamanlı servis durumunda değişiklik olması durumunda devreye girer
+            if (durum != (service2.Status.ToString().Substring(0, 1) == "R"))
+            {
+                listBox1.Items.Add("İzlenecek servis durum değiştirmiştir!!");
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            listBox2.Items.Clear();
+
+            // Şu an çalışan işlemlerin listesini al
+            Process[] currentProcesses = Process.GetProcesses();
+
+            // Şu an çalışan işlemleri listBox2'ye ekle
+            for (int i = 0; i < currentProcesses.Length; i++)
+            {
+                listBox2.Items.Add("Var olan>>" + currentProcesses[i].ProcessName.ToString());
+            }
+
+            listBox2.Items.Add("Farklı olanlar>>");
+
+            // dataGridView2'den işlemler listesini al
+            List<string> previousProcesses = new List<string>();
+            foreach (DataGridViewRow row in dataGridView2.Rows)
+            {
+                if (row.Cells[1].Value != null)
+                {
+                    previousProcesses.Add(row.Cells[1].Value.ToString());
+                }
+            }
+
+            // Şu an çalışan ve dataGridView2'de olmayan işlemleri bul ve listBox2'ye ekle
+            foreach (Process proc in currentProcesses)
+            {
+                if (!previousProcesses.Contains(proc.ProcessName))
+                {
+                    listBox2.Items.Add(proc.ProcessName);
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            listBox2.Items.Clear();
+            try
+            {
+                using (Socket listener = new Socket(AddressFamily.InterNetwork, SocketType.Raw, ProtocolType.IP))
+                {
+                    listener.Bind(new IPEndPoint(IPAddress.Parse(ipAdresi), 0));
+                    byte[] inValue = new byte[4] { 1, 0, 0, 0 };
+                    byte[] outValue = new byte[4] { 1, 0, 0, 0 };
+                    listener.IOControl(IOControlCode.ReceiveAll, inValue, outValue);
+                    byte[] buffer = new byte[1000000];
+                    int read = listener.Receive(buffer);
+                    if (read >= 20)
+                    {
+                        listBox2.Items.Add("Okunan paket bilgisi sayısı: " + read);
+                        string bilgiler = "";
+                        for (int i = 0; i < read; i++)
+                        {
+                            bilgiler = bilgiler + "(" + buffer[i] + ")" + ",";
+                        }
+                        listBox2.Items.Add("Elde edilen paket bilgisi: " + bilgiler);
+                        listBox2.Items.Add(Encoding.ASCII.GetString(buffer, 0, read));
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                listBox2.Items.Add("Hata: " + ex.Message);
+            }
+        }
+    }
+}
